9073e7d98528bbbc395c161ae3ca331f8c436194
watashisham <wata5hi@proton.me>
2024-06-04 19:06:44 -0300

    Atualização de Saldo

--- controle-fluxo/src/ResultadoEscolar.java
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -1,6 +1,6 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
-        int nota = 6;
+        int nota =8;
 
         if (nota>= 7)
         System.out.println("Aprovado");
